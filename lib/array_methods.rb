815be1f5dad4bf675c2aea1fe544f97a5bad08b6
Owen Cruse <ocruse7@gmail.com>
2019-10-24 17:03:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
+  array.length.times { |index|
     if array[index] == value_to_find
       return index
-    end 
+    }
   return nil
 end
 
